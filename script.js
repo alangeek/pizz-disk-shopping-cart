5752086c3f481d382450305e719f701055c2202c
alangeek <alanchristiancreator@hotmail.com>
2020-02-12 16:20:53 -0300

    pegando as informações do carrinho

--- script.js
+++ script.js
@@ -76,12 +76,8 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 	let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
-
 	let identifier = pizzaJson[modalKey].id+'@'+size;
-	console.log(identifier);
-
 	let key = cart.findIndex((item)=>item.identifier == identifier);
-
 	if(key > -1) {
 		cart[key].qt +=modalQt;
 	} else {
@@ -92,8 +88,21 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
 			qt:modalQt
 		});
 	}
-
-
-
+	updateCart();
 	closeModal();
 });
+
+function updateCart() {
+	if(cart.length > 0) {
+		c('aside').classList.add('show');
+		for(let i in cart) {
+
+			let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+
+			console.log(pizzaItem);
+		}
+
+	} else {
+		c('aside').classList.remove('show');
+	}
+}
